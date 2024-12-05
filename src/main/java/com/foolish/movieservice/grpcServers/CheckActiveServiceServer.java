5539207bd260b97e0bfbde0b3b857cab1bd8c6f1
MaivMinh <maivanminh.se@gmail.com>
2024-12-05 21:40:58 +0700

    Update proto file

--- src/main/java/com/foolish/movieservice/grpcServers/CheckActiveServiceServer.java
+++ src/main/java/com/foolish/movieservice/grpcServers/CheckActiveServiceServer.java
@@ -4,9 +4,9 @@ import com.foolish.movieservice.model.Movie;
 import com.foolish.movieservice.service.MovieService;
 import io.grpc.stub.StreamObserver;
 import lombok.AllArgsConstructor;
-import net.devh.boot.grpc.examples.lib.CheckActiveStateRequest;
-import net.devh.boot.grpc.examples.lib.CheckActiveStateResponse;
-import net.devh.boot.grpc.examples.lib.CheckActiveStateServiceGrpc;
+import net.devh.boot.grpc.examples.lib.ActiveStateRequest;
+import net.devh.boot.grpc.examples.lib.ActiveStateResponse;
+import net.devh.boot.grpc.examples.lib.CheckMovieActiveStateServiceGrpc;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +14,19 @@ import org.springframework.stereotype.Service;
 @GrpcService
 @Service
 @AllArgsConstructor
-public class CheckActiveServiceServer extends CheckActiveStateServiceGrpc.CheckActiveStateServiceImplBase {
+public class CheckActiveServiceServer extends CheckMovieActiveStateServiceGrpc.CheckMovieActiveStateServiceImplBase {
   private final MovieService movieService;
 
   @Override
-  public void doCheckActiveState(CheckActiveStateRequest request, StreamObserver<CheckActiveStateResponse> responseObserver) {
+  public void doCheckMovieActiveState(ActiveStateRequest request, StreamObserver<ActiveStateResponse> responseObserver) {
     // Thực hiện tìm kiếm movie và trạng thái isExpired của nó.
     int id = request.getId();
 
     Movie movie = movieService.findMovieById(id);
     if (movie == null || movie.getId() <= 0 || movie.getExpired()) {
-      responseObserver.onNext(CheckActiveStateResponse.newBuilder().setActive(false).build());
+      responseObserver.onNext(ActiveStateResponse.newBuilder().setActive(false).build());
     } else {
-      responseObserver.onNext(CheckActiveStateResponse.newBuilder().setActive(true).build());
+      responseObserver.onNext(ActiveStateResponse.newBuilder().setActive(true).build());
     }
     responseObserver.onCompleted();
   }
